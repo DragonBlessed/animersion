0f75a32e205b34a139043ea70e9b655f4b425a5a
DragonBlessed <christianvasil72@gmail.com>
2023-07-04 03:02:05 -0700

    added carousel structure for featured animes

--- src/App.js
+++ src/App.js
@@ -98,6 +98,28 @@ function Slogan() {
   );
 }
 
+const FeaturedCarousel = () => {
+  const [index, setIndex] = useState(0);
+  const length = 3;
+
+  const handlePrevious = () => {
+    const newIndex = index - 1;
+    setIndex(newIndex < 0 ? length - 1 : newIndex);
+  }
+  const handleNext = () => {
+    const newIndex = index + 1;
+    setIndex(newIndex >= length ? 0 : newIndex);
+  }
+  return (
+    <div className="carousel">
+      <button onClick={handlePrevious}>Previous</button>
+      <button onClick={handleNext}>Next</button>
+      <p>{index}</p>
+    </div>
+  )
+  }
+  
+
 function StartQuiz() {
   const [hovered, setHovered] = useState(false);
 
@@ -324,6 +346,7 @@ class App extends React.Component {
       <div className="App">
         <Header />
         <Slogan />
+        <FeaturedCarousel />
         <StartQuiz />
         <AnimeList
           airingList={airingList}
