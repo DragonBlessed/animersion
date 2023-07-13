10e8db23e71baa31451122d254ddf0bbe939a7a5
DragonBlessed <christianvasil72@gmail.com>
2023-07-12 23:54:44 -0700

    bug fixed with load more displaying both lists instead of one

--- src/App.js
+++ src/App.js
@@ -7,8 +7,6 @@ import 'tailwindcss/tailwind.css';
 import 'react-responsive-carousel/lib/styles/carousel.min.css';
 import { Carousel } from 'react-responsive-carousel';
 
-
-
 function Header() {
   const [showMenu, setShowMenu] = useState(false);
   const [hoveredButton, setHoveredButton] = useState(null);
@@ -53,8 +51,8 @@ function Header() {
                 id="home"
                 className={`normalButton ${hoveredButton === 'home' ? 'hover' : ''}`}
                 onClick={() => setHoveredButton(null)}
-                onMouseEnter={() => setHoveredButton('home')}
-                onMouseLeave={() => setHoveredButton(null)}
+                onMouseEnter={() => handleButtonMouseEnter('home')}
+                onMouseLeave={handleButtonMouseLeave}
               >
                 Home
               </button>
@@ -62,8 +60,8 @@ function Header() {
                 id="anime-genres"
                 className={`normalButton ${hoveredButton === 'anime-genres' ? 'hover' : ''}`}
                 onClick={() => setHoveredButton(null)}
-                onMouseEnter={() => setHoveredButton('anime-genres')}
-                onMouseLeave={() => setHoveredButton(null)}
+                onMouseEnter={() => handleButtonMouseEnter('anime-genres')}
+                onMouseLeave={handleButtonMouseLeave}
               >
                 Anime Genres
               </button>
@@ -71,8 +69,8 @@ function Header() {
                 id="mal-profile"
                 className={`normalButton ${hoveredButton === 'mal-profile' ? 'hover' : ''}`}
                 onClick={() => setHoveredButton(null)}
-                onMouseEnter={() => setHoveredButton('mal-profile')}
-                onMouseLeave={() => setHoveredButton(null)}
+                onMouseEnter={() => handleButtonMouseEnter('mal-profile')}
+                onMouseLeave={handleButtonMouseLeave}
               >
                 MAL Profile
               </button>
@@ -80,8 +78,8 @@ function Header() {
                 id="faq"
                 className={`normalButton ${hoveredButton === 'faq' ? 'hover' : ''}`}
                 onClick={() => setHoveredButton(null)}
-                onMouseEnter={() => setHoveredButton('faq')}
-                onMouseLeave={() => setHoveredButton(null)}
+                onMouseEnter={() => handleButtonMouseEnter('faq')}
+                onMouseLeave={handleButtonMouseLeave}
               >
                 Faq
               </button>
@@ -103,6 +101,7 @@ function Slogan() {
 
 const FeaturedCarousel = () => {
   const [carouselData, setCarouselData] = useState([]);
+  const [intervalId, setIntervalId] = useState(null);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -110,11 +109,11 @@ const FeaturedCarousel = () => {
         const response = await axios.get('https://api.jikan.moe/v4/top/anime', {
           params: {
             filter: 'favorite',
-            limit: 10,
+            limit: 1,
           },
         });
         const data = response.data.data;
-        setCarouselData(data);
+        setCarouselData((prevData) => [...prevData, ...data]);
       } catch (error) {
         console.log(error);
       }
@@ -123,11 +122,18 @@ const FeaturedCarousel = () => {
     fetchData();
 
     const timer = setInterval(fetchData, 60 * 5000);
+    setIntervalId(timer);
 
     return () => {
       clearInterval(timer);
     };
   }, []);
+
+  useEffect(() => {
+    if (carouselData.length >= 5) {
+      clearInterval(intervalId);
+    }
+  }, [carouselData]);
 
   return (
     <div>
@@ -146,7 +152,6 @@ const FeaturedCarousel = () => {
     </div>
   );
 };
-  
 
 function StartQuiz() {
   const [hovered, setHovered] = useState(false);
@@ -183,115 +188,49 @@ function StartQuiz() {
   );
 }
 
-function fetchAiringRankings(page) {
-  return axios
-    .get('https://api.jikan.moe/v4/top/anime', {
-      params: {
-        filter: 'airing',
-        limit: 10,
-        page: page,
-      },
-    })
-    .then((res) => {
-      console.log(res.data);
-      return res.data.data;
-    })
-    .catch((error) => {
-      console.log(error);
-      return [];
-    });
-}
-
-function fetchPopularRankings(page) {
-  return axios
-    .get('https://api.jikan.moe/v4/top/anime', {
-      params: {
-        filter: 'bypopularity',
-        limit: 10,
-        page: page,
-      },
-    })
-    .then((res) => {
-      console.log(res.data);
-      return res.data.data;
-    })
-    .catch((error) => {
-      console.log(error);
-      return [];
-    });
-}
-
-const AnimeList = ({ airingList, popularList, fetchMoreAiring, fetchMorePopular }) => {
+const AnimeList = () => {
   const [airingPage, setAiringPage] = useState(1);
   const [popularPage, setPopularPage] = useState(1);
   const [airingListData, setAiringListData] = useState([]);
   const [popularListData, setPopularListData] = useState([]);
 
-  useEffect(() => {
-    const fetchAiringRankings = async () => {
-      try {
-        const data = await fetchAiringData(airingPage);
-        setAiringListData((prevList) => [...prevList, ...data]);
-      } catch (error) {
-        console.log(error);
-      }
-    };
-
-    const fetchPopularRankings = async () => {
-      try {
-        const data = await fetchPopularData(popularPage);
-        setPopularListData((prevList) => [...prevList, ...data]);
-      } catch (error) {
-        console.log(error);
-      }
-    };
-
-    fetchAiringRankings();
-    fetchPopularRankings();
-
-    const timer = setInterval(() => {
-      fetchAiringRankings();
-      fetchPopularRankings();
-    }, 5 * 60 * 1000);
-
-    return () => {
-      clearInterval(timer);
-    };
-  }, [airingPage, popularPage]);
-
-  const fetchAiringData = async (page) => {
+useEffect(() => {
+  const fetchAiringRankings = async () => {
     try {
       const response = await axios.get('https://api.jikan.moe/v4/top/anime', {
         params: {
           filter: 'airing',
           limit: 10,
-          page: page,
+          page: airingPage,
         },
       });
       const data = response.data.data;
-      return data;
+      setAiringListData((prevList) => [...prevList, ...data]);
     } catch (error) {
       console.log(error);
-      return [];
     }
   };
+  fetchAiringRankings();
+}, [airingPage]);
 
-  const fetchPopularData = async (page) => {
+useEffect(() => {
+  const fetchPopularRankings = async () => {
     try {
       const response = await axios.get('https://api.jikan.moe/v4/top/anime', {
         params: {
           filter: 'bypopularity',
           limit: 10,
-          page: page,
+          page: popularPage,
         },
       });
       const data = response.data.data;
-      return data;
+      setPopularListData((prevList) => [...prevList, ...data]);
     } catch (error) {
       console.log(error);
-      return [];
     }
   };
+  fetchPopularRankings();
+}, [popularPage]);
 
   const handleAiringNextPage = () => {
     const nextPage = airingPage + 1;
@@ -343,153 +282,17 @@ function Footer() {
   )
 }
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      airingList: [],
-      popularList: [],
-      animeImages: [],
-      airingPage: 1,
-      popularPage: 1,
-    };
-    this.fetchAnimeImages = this.fetchAnimeImages.bind(this);
-    this.fetchMoreAiring = this.fetchMoreAiring.bind(this);
-    this.fetchMorePopular = this.fetchMorePopular.bind(this);
-  }
-
-  componentDidMount() {
-    this.fetchAnimeImages()
-      .then((animeImages) => {
-        this.setState({ animeImages });
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-
-    this.fetchAiringRankings()
-      .then((airingList) => {
-        this.setState({ airingList });
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-
-    this.fetchPopularRankings()
-      .then((popularList) => {
-        this.setState({ popularList });
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  }
-
-  fetchAnimeImages() {
-    return axios
-      .get('https://api.jikan.moe/v4/top/anime', {
-        params: {
-          filter: 'favorite',
-          limit: 3,
-          page: 1,
-        },
-      })
-      .then((res) => {
-        return res.data.data;
-      })
-      .catch((error) => {
-        console.log(error);
-        return [];
-      });
-  }
-
-  fetchAiringRankings(page) {
-    const { airingPage } = this.state;
-    return axios
-      .get('https://api.jikan.moe/v4/top/anime', {
-        params: {
-          filter: 'airing',
-          limit: 10,
-          page: airingPage,
-        },
-      })
-      .then((res) => {
-        console.log(res.data);
-        return res.data.data;
-      })
-      .catch((error) => {
-        console.log(error);
-        return [];
-      });
-  }
-
-  fetchPopularRankings(page) {
-    const { popularPage } = this.state;
-    return axios
-      .get('https://api.jikan.moe/v4/top/anime', {
-        params: {
-          filter: 'bypopularity',
-          limit: 10,
-          page: popularPage,
-        },
-      })
-      .then((res) => {
-        console.log(res.data);
-        return res.data.data;
-      })
-      .catch((error) => {
-        console.log(error);
-        return [];
-      });
-  }
-
-  fetchMoreAiring() {
-    const { airingPage, airingList } = this.state;
-    const nextPage = airingPage + 1;
-    this.fetchAiringRankings(nextPage)
-      .then((newAiringList) => {
-        this.setState({
-          airingList: [...airingList, ...newAiringList],
-          airingPage: nextPage,
-        });
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  }
-
-  fetchMorePopular() {
-    const { popularPage, popularList } = this.state;
-    const nextPage = popularPage + 1;
-    this.fetchPopularRankings(nextPage)
-      .then((newPopularList) => {
-        this.setState({
-          popularList: [...popularList, ...newPopularList],
-          popularPage: nextPage,
-        });
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  }
-
-  render() {
-    const { airingList, popularList, animeImages } = this.state;
-    return (
-      <div className="App">
-        <Header />
-        <Slogan />
-        <FeaturedCarousel animeImages={animeImages} />
-        <StartQuiz />
-        <AnimeList
-          airingList={airingList}
-          popularList={popularList}
-          fetchMoreAiring={this.fetchMoreAiring}
-          fetchMorePopular={this.fetchMorePopular}
-        />
-        <Footer />
-      </div>
-    );
-  }
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Slogan />
+      <FeaturedCarousel />
+      <StartQuiz />
+      <AnimeList />
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
