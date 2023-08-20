05f290a4568cb84195587821fa67ef9ae08e5aee
DragonBlessed <christianvasil72@gmail.com>
2023-08-20 00:10:28 -0700

    sidebar issues

--- api/jikan.js
+++ api/jikan.js
@@ -1,7 +1,13 @@
-import axios from 'axios';
+let cache = {};
+let cacheTimestamp = null;
 
 export default async (req, res) => {
   try {
+    // Check if the cache is still valid
+    if (cacheTimestamp && Date.now() - cacheTimestamp < 10 * 60 * 1000) {
+      return res.status(200).json(cache);
+    }
+
     // Fetching the top anime by airing
     const airingResponse = await axios.get('https://api.jikan.moe/v4/top/anime', {
       params: {
@@ -15,6 +21,10 @@ export default async (req, res) => {
     const newsData = newsResponse.data.data;
 
     res.status(200).json(newsData);
+
+    // Update the cache
+    cache = newsData;
+    cacheTimestamp = Date.now();
   } catch (error) {
     console.log(error);
     res.status(500).json({ error: error.message || 'Failed to fetch anime news' });
