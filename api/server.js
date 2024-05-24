7c58ebf0ba199713c426738caa43f474fba8d02e
DragonBlessed <christianvasil72@gmail.com>
2024-05-24 01:59:04 -0700

    Fixed backend to work with databases for MongoDB and Vercel

--- api/server.js
+++ api/server.js
@@ -4,6 +4,8 @@ import { connectToDatabase } from './db.js';
 import saveQuizAnswersRouter from './saveQuizAnswers.js';
 import userAnimelistRouter from './userAnimelist.js';
 import dotenv from 'dotenv';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
 dotenv.config();
 
@@ -12,10 +14,35 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const connectToDatabase = async () => {
+  try {
+    const mongoDBUsername = process.env.MONGO_DB_USERNAME;
+    const mongoDBPassword = process.env.MONGO_DB_PASSWORD;
+    const mongoUri = `mongodb+srv://${mongoDBUsername}:${mongoDBPassword}@animersion.vhua5vh.mongodb.net/?retryWrites=true&w=majority&appName=animersion`;
+
+    console.log("MongoDB URI: ", mongoUri);
+
+    const client = new MongoClient(mongoUri, {
+      serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+      }
+    });
+
+    await client.connect();
+    console.log("Connected successfully to server");
+  } catch (err) {
+    console.error('Failed to connect to the database', err);
+  }
+};
 
 connectToDatabase().then(() => {
-  app.use('/api/jikan', require('./jikan.js'));
-  app.use('/api/mal', require('./mal.js'));
+  import(path.join(__dirname, './jikan.js')).then(module => app.use('/api/jikan', module.default));
+  import(path.join(__dirname, './mal.js')).then(module => app.use('/api/mal', module.default));
   app.use('/api/saveQuizAnswers', saveQuizAnswersRouter);
   app.use('/api/user-animelist', userAnimelistRouter);
 
