e48792a96ffde566698f944619ab069d3ed21fc4
DragonBlessed <christianvasil72@gmail.com>
2024-05-24 02:06:52 -0700

    Changed server.js to fix bugs with vercel

--- api/server.js
+++ api/server.js
@@ -4,8 +4,6 @@ import { connectToDatabase } from './db.js';
 import saveQuizAnswersRouter from './saveQuizAnswers.js';
 import userAnimelistRouter from './userAnimelist.js';
 import dotenv from 'dotenv';
-import path from 'path';
-import { fileURLToPath } from 'url';
 
 dotenv.config();
 
@@ -16,29 +14,6 @@ app.use(express.json());
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-
-const connectToDatabase = async () => {
-  try {
-    const mongoDBUsername = process.env.MONGO_DB_USERNAME;
-    const mongoDBPassword = process.env.MONGO_DB_PASSWORD;
-    const mongoUri = `mongodb+srv://${mongoDBUsername}:${mongoDBPassword}@animersion.vhua5vh.mongodb.net/?retryWrites=true&w=majority&appName=animersion`;
-
-    console.log("MongoDB URI: ", mongoUri);
-
-    const client = new MongoClient(mongoUri, {
-      serverApi: {
-        version: ServerApiVersion.v1,
-        strict: true,
-        deprecationErrors: true,
-      }
-    });
-
-    await client.connect();
-    console.log("Connected successfully to server");
-  } catch (err) {
-    console.error('Failed to connect to the database', err);
-  }
-};
 
 connectToDatabase().then(() => {
   import(path.join(__dirname, './jikan.js')).then(module => app.use('/api/jikan', module.default));
